b893dda129de6e7ec5038f86ea70b5c332be1e6e
fitriananurafifa8 <48310096+fitriananurafifa8@users.noreply.github.com>
2019-10-03 16:30:41 +0700

    Add files via upload

---
+++ tugas2gojek/app/src/main/java/com/fitriana/tugas2gojek/gofood.java
@@ -0,0 +1,30 @@
+package com.fitriana.tugas2gojek;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fitriana.tugas2gojek.MainActivity;
+
+public class gofood extends AppCompatActivity {
+
+    Button btnkembali;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gofood);
+
+        btnkembali = findViewById(R.id.kembali);
+
+        btnkembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (gofood.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
